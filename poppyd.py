7232e8701e122a84af0df1b50a84c1854b39d886
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-03-21 15:16:46 +0100

    Simple daemon that runs poppy-services using a config for parameters.

---
+++ poppyd.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import os
+import yaml
+import signal
+
+from subprocess import Popen
+
+
+class Daemon(object):
+    def __init__(self, cmd, pidfile, logfile):
+        self.cmd = cmd
+        self.pidfile = os.path.abspath(pidfile)
+        self.logfile = os.path.abspath(logfile)
+
+    def start(self):
+        if 'running' in self.status():
+            raise SystemError('pidfile {} already exist. '
+                              'Daemon already running?'.format(self.pidfile))
+
+        with open(self.logfile, 'w') as log:
+            p = Popen(self.cmd, stdout=log, stderr=log)
+
+            with open(self.pidfile, 'w') as f:
+                f.write('{}'.format(p.pid))
+
+        return('Poppy daemon is now running!')
+
+    def stop(self):
+        if 'stopped' in self.status():
+            raise SystemError('pidfile {} does not exist. '
+                              'Daemon already stopped?'.format(self.pidfile))
+
+        with open(self.pidfile) as f:
+            pid = int(f.read())
+
+        os.kill(pid, signal.SIGTERM)
+        os.remove(self.pidfile)
+
+        return('Poppy daemon is now stopped!')
+
+    def restart(self):
+        if 'running' in self.status():
+            self.stop()
+        self.start()
+
+        return('Poppy daemon has been restarted!')
+
+    def status(self):
+        return 'Poppy daemon is {}.'.format('running'
+                                            if os.path.exists(self.pidfile) else
+                                            'stopped')
+
+    def force_clean(self):
+        os.remove(self.pidfile)
+
+
+class PoppyDaemon(Daemon):
+    def __init__(self, configfile, pidfile):
+        with open(configfile) as f:
+            config = yaml.load(f)
+
+        cmd = [
+            'poppy-services',
+            config['robot']['creature'],
+            '--snap',
+            '--no-browser',
+        ]
+
+        if not config['robot']['camera']:
+            cmd += ['--disable-camera']
+
+        if 'use-dummy' in config['robot'] and config['robot']['use-dummy']:
+            cmd += ['--poppy-simu']
+
+        Daemon.__init__(self, cmd, pidfile, config['info']['logfile'])
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Poppy daemon',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('command', type=str,
+                        choices=('start', 'stop', 'restart', 'status'),
+                        help='possible commands')
+    parser.add_argument('--configfile', type=str,
+                        default=os.path.expanduser('~/.poppy_config.yaml'),
+                        help='used config file')
+    parser.add_argument('--pidfile', type=str,
+                        default='/tmp/poppyd-pid.lock',
+                        help='used pid file')
+    args = parser.parse_args()
+
+    poppyd = PoppyDaemon(args.configfile, args.pidfile)
+    print(getattr(poppyd, args.command)())
