fc8b9a04e80b44c9ab2d0e269b88e084ca3e1f4e
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-03-21 16:41:13 +0100

    Add the puppet master core module. It allows you to start/stop/run/modify the basics needed for your poppy robot.

---
+++ config.py
@@ -0,0 +1,60 @@
+import yaml
+
+
+class Config(object):
+    def __init__(self, dict, filename=None):
+        object.__setattr__(self, '_{}__config'.format(type(self).__name__), dict)
+        object.__setattr__(self, '_{}__file'.format(type(self).__name__), filename)
+
+    def close(self):
+        config = self.__dict__['_{}__config'.format(type(self).__name__)]
+        filename = self.__dict__['_{}__file'.format(type(self).__name__)]
+
+        if filename is not None:
+            with open(filename, 'w') as f:
+                f.write(yaml.dump(config))
+
+    @classmethod
+    def from_file(cls, filename):
+        with open(filename) as f:
+            return cls(yaml.load(f), filename)
+
+    def __repr__(self):
+        return str(self.__config)
+
+    def __getattr__(self, key):
+        value = self.__config[key]
+        if isinstance(value, dict):
+            return Config(value)
+
+        return value
+
+    def __setattr__(self, key, value):
+        config = self.__dict__['_{}__config'.format(type(self).__name__)]
+        config[key] = value
+
+
+def attrsetter(item):
+    def resolve_attr(obj, attr):
+        if not attr:
+            return obj
+        for name in attr.split('.'):
+            obj = getattr(obj, name)
+        return obj
+
+    def g(obj, value):
+        var_path, _, var_name = item.rpartition('.')
+        setattr(resolve_attr(obj, var_path), var_name, value)
+
+    return g
+
+if __name__ == '__main__':
+    from contextlib import closing
+
+    with closing(Config.from_file('/tmp/config.yaml')) as c:
+        print(c)
+        print(c.robot)
+        print(c.robot.name)
+
+        c.robot.dummy = 42
+        print(c.robot)
---
+++ puppet_master.py
@@ -0,0 +1,85 @@
+import os
+
+from subprocess import call
+from contextlib import closing
+
+from config import Config
+from poppyd import PoppyDaemon
+from toolbox import attrsetter
+
+
+class PuppetMaster(object):
+    def __init__(self, DaemonCls, configfile, pidfile, logfile):
+        self.configfile = os.path.abspath(configfile)
+        self.pidfile = os.path.abspath(pidfile)
+        self.logfile = os.path.abspath(logfile)
+
+        self.daemon = DaemonCls(self.configfile, self.pidfile, self.logfile)
+
+        self.config_handlers = {
+            'robot.camera': lambda _: self.restart(),
+            'robot.name': self._change_hostname,
+        }
+
+    def start(self):
+        self.daemon.start()
+
+    @property
+    def running(self):
+        return 'running' in self.daemon.status()
+
+    def stop(self):
+        try:
+            self.daemon.stop()
+        except OSError:
+            self.force_clean()
+
+    def restart(self):
+        if self.running:
+            self.stop()
+
+        self.start()
+
+    def force_clean(self):
+        self.daemon.force_clean()
+
+    @property
+    def config(self):
+        return Config.from_file(self.configfile)
+
+    def update_config(self, key, value):
+        with closing(self.config) as c:
+            attrsetter(key)(c, value)
+
+        if key in self.config_handlers:
+            self.config_handlers[key](value)
+
+    def update(self):
+        raise NotImplementedError
+
+    @property
+    def is_updating(self):
+        raise NotImplementedError
+
+    def _change_hostname(self, name):
+        call(['sudo', 'raspi-config', '--change-hostname', name])
+        call(['sudo', 'service', 'avahi-daemon', 'restart'])
+        self.restart()
+
+
+if __name__ == '__main__':
+    import sys
+
+    configfile = os.path.expanduser('~/.poppy_config.yaml')
+    pidfile = '/tmp/puppet-master-pid.lock'
+    logfile = '/tmp/puppet-master.log'
+
+    puppet_master = PuppetMaster(DaemonCls=PoppyDaemon,
+                                 configfile=configfile,
+                                 pidfile=pidfile,
+                                 logfile=logfile)
+
+    if sys.argv[1] == 'start':
+        puppet_master.start()
+    elif sys.argv[1] == 'stop':
+        puppet_master.stop()
